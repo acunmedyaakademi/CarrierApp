7935285c44c2c488f9146c4ce6f6ef6e374c37c3
Furkan Demirtaş <furkan-demirtas@outlook.com>
2025-03-26 19:07:16 +0300

    Home Page tasarımı, supabase'den veri çekimi ve listeleme tamamlandı

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,21 +1,99 @@
+import "../assets/HomePage.css";
+
 import Layout from "./Layout";
 import { supabase } from "../../supabaseClient";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [isHistory, setHistory] = useState(false);
+  const [orders, setOrders] = useState([]);
+  const [filteredOrders, setFilteredOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const { data, error } = await supabase
+        .from("order_details")
+        .select("*, orders(*, status(*)), products(*)");
+
+      if (error) {
+        console.error("Error fetching orders:", error);
+      } else {
+        setOrders(data);
+      }
+    };
+
+    fetchOrders();
+  }, []);
+
+  useEffect(() => {
+    setFilteredOrders(
+      isHistory
+        ? orders.filter((x) => x.orders.status.name === "received")
+        : orders.filter((x) => x.orders.status.name !== "received")
+    );
+  }, [isHistory, orders]);
+
   return (
     <Layout>
       <div className="home-container">
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the home page content.</p>
-        <button
-          onClick={async () => {
-            const { error } = await supabase.auth.signOut();
-            if (error) console.log("Error signing out:", error.message);
-          }}
-        >
-          Sign Out
-        </button>
+        <h1 className="page-header">Order</h1>
+        <div className="home-orders-tab-btns">
+          <button
+            className={"home-orders-tab-btn" + (isHistory ? "" : " active")}
+            onClick={() => setHistory(false)}
+          >
+            Upcoming
+          </button>
+          <button
+            className={"home-orders-tab-btn" + (isHistory ? " active" : "")}
+            onClick={() => setHistory(true)}
+          >
+            History
+          </button>
+        </div>
+        <div className="home-orders-list">
+          {filteredOrders.map((order) => (
+            <Order key={order.id} {...order} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
 }
+
+const deliveryDateFormat = (date) => {
+  const dateObj = new Date(date);
+  const day = dateObj.getDate();
+  const month = dateObj.toLocaleString("default", { month: "short" });
+  return `${day}, ${month}`;
+};
+
+const Order = ({ orders, products }) => {
+  return (
+    <div className="home-order-card">
+      <img src={products.img} />
+      <div className="home-order-card-info">
+        <div className="home-order-status">
+          <span>{orders.status.name}</span>
+          <span>{deliveryDateFormat(orders.created_at)}</span>
+        </div>
+        <h2 className="home-order-name">{products.name}</h2>
+      </div>
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M6.18194 4.18185C6.35767 4.00611 6.6426 4.00611 6.81833 4.18185L9.81833 7.18185C9.90272 7.26624 9.95013 7.3807 9.95013 7.50005C9.95013 7.6194 9.90272 7.73386 9.81833 7.81825L6.81833 10.8182C6.6426 10.994 6.35767 10.994 6.18194 10.8182C6.0062 10.6425 6.0062 10.3576 6.18194 10.1819L8.86374 7.50005L6.18194 4.81825C6.0062 4.64251 6.0062 4.35759 6.18194 4.18185Z"
+          fill="currentColor"
+          fillRule="evenodd"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+    </div>
+  );
+};
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,13 +5,88 @@ export default function Layout({ children }) {
       <nav>
         <ul>
           <li>
-            <a href="/">Orders</a>
+            <a href="/">
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M8 0L0 6V8H1V15H4V10H7V15H15V8H16V6L14 4.5V1H11V2.25L8 0ZM9 10H12V13H9V10Z"
+                    fill="#333"
+                  ></path>
+                </g>
+              </svg>
+              <span>Deliveries</span>
+            </a>
           </li>
           <li>
-            <a href="/my-deliveries">My Deliveries</a>
+            <a href="/my-deliveries">
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="0 0 48 48"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#333"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <g id="Layer_2" data-name="Layer 2">
+                    <g id="invisible_box" data-name="invisible box">
+                      <rect width="48" height="48" fill="none"></rect>
+                    </g>
+                    <g id="Layer_7" data-name="Layer 7">
+                      <g>
+                        <path d="M37.7,11.1A3,3,0,0,0,35.4,10H34.2l.3-1.7A3.1,3.1,0,0,0,33.9,6a3.2,3.2,0,0,0-2.2-1H7.9A2.1,2.1,0,0,0,5.8,6.7,2,2,0,0,0,7.8,9h7.3A3,3,0,0,1,18,12.5L15.6,26.3a3,3,0,0,1-2.9,2.5H4.8a2,2,0,0,0-2,1.6L2,34.7A2.8,2.8,0,0,0,2.7,37a2.8,2.8,0,0,0,2.1,1H7.3a7,7,0,0,0,13.4,0h4.6a7,7,0,0,0,13.4,0h2a3.2,3.2,0,0,0,3.1-2.7L46,22.5ZM14,39a3,3,0,0,1-3-3,3,3,0,0,1,6,0A3,3,0,0,1,14,39Zm18,0a3,3,0,0,1-3-3,3,3,0,0,1,6,0A3,3,0,0,1,32,39Zm.1-17,1.4-8h1.3l5.9,8Z"></path>
+                        <path d="M4,15H14a2,2,0,0,0,0-4H4a2,2,0,0,0,0,4Z"></path>
+                        <path d="M15,19a2,2,0,0,0-2-2H5a2,2,0,0,0,0,4h8A2,2,0,0,0,15,19Z"></path>
+                        <path d="M6,23a2,2,0,0,0,0,4h6a2,2,0,0,0,0-4Z"></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+              <span>My Deliveries</span>
+            </a>
           </li>
           <li>
-            <a href="/profile">Account</a>
+            <a href="/profile">
+              <svg
+                fill="#333"
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <title>Account Settings</title>
+                  <path d="M9.6,3.32a3.86,3.86,0,1,0,3.86,3.85A3.85,3.85,0,0,0,9.6,3.32M16.35,11a.26.26,0,0,0-.25.21l-.18,1.27a4.63,4.63,0,0,0-.82.45l-1.2-.48a.3.3,0,0,0-.3.13l-1,1.66a.24.24,0,0,0,.06.31l1,.79a3.94,3.94,0,0,0,0,1l-1,.79a.23.23,0,0,0-.06.3l1,1.67c.06.13.19.13.3.13l1.2-.49a3.85,3.85,0,0,0,.82.46l.18,1.27a.24.24,0,0,0,.25.2h1.93a.24.24,0,0,0,.23-.2l.18-1.27a5,5,0,0,0,.81-.46l1.19.49c.12,0,.25,0,.32-.13l1-1.67a.23.23,0,0,0-.06-.3l-1-.79a4,4,0,0,0,0-.49,2.67,2.67,0,0,0,0-.48l1-.79a.25.25,0,0,0,.06-.31l-1-1.66c-.06-.13-.19-.13-.31-.13L19.5,13a4.07,4.07,0,0,0-.82-.45l-.18-1.27a.23.23,0,0,0-.22-.21H16.46M9.71,13C5.45,13,2,14.7,2,16.83v1.92h9.33a6.65,6.65,0,0,1,0-5.69A13.56,13.56,0,0,0,9.71,13m7.6,1.43a1.45,1.45,0,1,1,0,2.89,1.45,1.45,0,0,1,0-2.89Z"></path>
+                </g>
+              </svg>
+              <span>Account</span>
+            </a>
           </li>
         </ul>
       </nav>
